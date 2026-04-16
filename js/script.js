2cfc9c627518278bd27cd89479cb787e8e868f8d
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-04-16 19:08:40 +0800

    Created the GameBoard

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
-let x = 1
-console.log(x);
+const GameBoard = (function () {
+    const gameBoard = [];
+
+    function createTile (pos) {
+        return {
+            position: pos,
+            mark: ""
+        }
+    }
+
+    function createBoard () {
+        for (let i = 0; i < 9; i++) {
+            gameBoard.push(createTile(i));
+        }
+    }
+
+    return {
+        init: function() {
+            createBoard();
+            return gameBoard;
+        }
+    }
+})();
+
+console.log(GameBoard.init());
