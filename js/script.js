a0150d76508855993594ed03daad7ab4f655d62c
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-04-18 17:06:51 +0800

    Added a board scanner that finds unmarked tiles

--- js/script.js
+++ js/script.js
@@ -2,12 +2,24 @@ const Game = (function () {
 
     function start() {
         const board = GameBoard.init();
+        let freeTiles = GameBoard.scan(board);
         const player1 = Players.init()[0];
         const player2 = Players.init()[1];
 
-        console.table(board);
-        console.table(player1);
-        console.table(player2);
+        // console.table(board);
+        // console.table(player1);
+        // console.table(player2);
+        console.log(freeTiles);
+       
+    }
+
+    function playerScore(player) {
+        player.score++;
+        console.log(`${player.name} wins!`);
+    }
+
+    function playerChoice() {
+
     }
 
     return {
@@ -33,7 +45,6 @@ const Players = (function () {
 
 const GameBoard = (function () {
     const gameBoard = [];
-
     function createTile(pos) {
         return {
             position: pos,
@@ -47,13 +58,69 @@ const GameBoard = (function () {
         }
     }
 
+    function clearBoard() {
+        gameBoard.length = 0;
+    }
+
+    function scan(board) {
+        let freeTiles = board.filter((tile) => {
+            return tile.mark === "";
+        });
+
+        return freeTiles;
+    }
+
     function init() {
+        clearBoard();
         createBoard();
         return gameBoard;
     }
 
+    function test() {
+        return [
+            {
+                "position": 0,
+                "mark": "X"
+            },
+            {
+                "position": 1,
+                "mark": ""
+            },
+            {
+                "position": 2,
+                "mark": ""
+            },
+            {
+                "position": 3,
+                "mark": "O"
+            },
+            {
+                "position": 4,
+                "mark": ""
+            },
+            {
+                "position": 5,
+                "mark": ""
+            },
+            {
+                "position": 6,
+                "mark": ""
+            },
+            {
+                "position": 7,
+                "mark": ""
+            },
+            {
+                "position": 8,
+                "mark": ""
+            }
+        ]
+    }
+
     return {
-        init
+        init,
+        scan,
+        test
     }
 })();
 
