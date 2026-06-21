d1bad81ff4dff3847d7615c3c64424e169e9704c
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-06-21 10:52:09 +0800

    Added a template and a testTemplate

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-let x = 1
-console.log(x);
+const gameBoard = (() => {
+
+    // THE GAMEBOARD
+    //       *  |  *  |  *    1. Positions are exactly how it says in template = [{ postion:"position",... }
+    //     -----+-----+-----        * This is for mine own readability
+    //       *  |  *  |  *    2. Marks are either "O" or "X"
+    //     -----+-----+-----
+    //       *  |  *  |  *   
+
+    const template =  [
+        { position: "top-left", mark: "" }, { position: "top-center", mark: "" }, { position: "top-right", mark: "" },
+        { position: "center-left", mark: "" }, { position: "center-center", mark: "" }, { position: "center-right", mark: "" },
+        { position: "bottom-left", mark: "" }, { position: "bottom-center", mark: "" }, { position: "bottom-right", mark: "" }
+    ]
+
+    const testTemplate = [
+        { position: "top-left", mark: "X" }, { position: "top-center", mark: "X" }, { position: "top-right", mark: "O" },
+        { position: "center-left", mark: "O" }, { position: "center-center", mark: "X" }, { position: "center-right", mark: "" },
+        { position: "bottom-left", mark: "X" }, { position: "bottom-center", mark: "O" }, { position: "bottom-right", mark: "O" }
+    ]
+
+    
+    
+})();
