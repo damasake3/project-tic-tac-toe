d36875230daf27586529deb1342c8c72c6bf0b2d
damasake3 <171648817+damasake3@users.noreply.github.com>
2026-06-21 12:21:38 +0800

    Added an Init and Display of the Gameboard(console)

--- js/script.js
+++ js/script.js
@@ -19,6 +19,45 @@ const gameBoard = (() => {
         { position: "bottom-left", mark: "X" }, { position: "bottom-center", mark: "O" }, { position: "bottom-right", mark: "O" }
     ]
 
-    
+    const board = [];
+
+    function init() {
+        board.push(...testTemplate);
+        return board;
+    }
+
+    function display(board) {
+        let top, center, bot;
+        const between = "-----+-----+-----";
+
+        console.log(between);
+        console.log(`  ${check(board[0].mark)}  |  ${check(board[1].mark)}  |  ${check(board[2].mark)}  `);
+        console.log(between);
+        console.log(`  ${check(board[3].mark)}  |  ${check(board[4].mark)}  |  ${check(board[5].mark)}  `);
+        console.log(between);
+        console.log(`  ${check(board[6].mark)}  |  ${check(board[7].mark)}  |  ${check(board[8].mark)}  `);
+
+        function check (boardMark){
+            if (boardMark === "") {
+                return "*";
+            }
+            else {
+                return boardMark;
+            }
+        }
+
+    }
+
+    return {
+        init,
+        display
+    }
     
 })();
+
+// Transfer these to the `Game=(()=>{})();` or `Player=(()=>{})();`later, if applicable. 
+const gameboard = gameBoard.init();
+console.log(gameboard);
+
+gameBoard.display(gameboard);
+
